bd800ecd643a7ad0e55bae7d4498d8e9984e51d5
yarendemirol <1907yarendemirol@gmail.com>
2025-12-31 01:43:59 +0300

    Update drift_detector.py

--- src/monitoring/drift_detector.py
+++ src/monitoring/drift_detector.py
@@ -3,10 +3,15 @@ import numpy as np
 from sklearn.metrics import accuracy_score
 
 def check_model_degradation(y_true, y_pred, threshold=0.90):
+    """
+    Model performansını kontrol eder.
+    """
     acc = accuracy_score(y_true, y_pred)
     print(f"Current Model Accuracy: {acc}")
+
     if acc < threshold:
         print("!!! WARNING: Performance Degradation Detected !!!")
         print("Action: Triggering Algorithmic Fallback to older version.")
         return True
     return False
+    
